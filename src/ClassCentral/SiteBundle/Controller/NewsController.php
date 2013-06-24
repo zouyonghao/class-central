5c7cbd37824c55a2b3fbb22b5a889540d32b1cbc
Dhawal Shah <dhawalhshah@gmail.com>
2013-06-23 20:54:08 -0700

    Specifying the limit on the news query correctly

--- src/ClassCentral/SiteBundle/Controller/NewsController.php
+++ src/ClassCentral/SiteBundle/Controller/NewsController.php
@@ -35,7 +35,7 @@ class NewsController extends Controller
         $query->add('select', 'n')
             ->add('from', 'ClassCentralSiteBundle:News n')
             ->add('orderBy', 'n.id DESC')
-            ->add('limit', 5);
+            ->setMaxResults(5);
         $news = $query->getQuery()->getResult();
 
         $newsArray = array();
