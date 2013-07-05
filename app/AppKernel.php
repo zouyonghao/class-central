7e36494b67dd1bdcfe8fe04fd0a01994745ba727
Dhawal Shah <dhawalhshah@gmail.com>
2013-07-05 01:20:48 -0700

    More changes to make it compatible with symonfy 2.3

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -13,13 +13,11 @@ class AppKernel extends Kernel
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Symfony\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
-            new ClassCentral\StaticBundle\ClassCentralStaticBundle(),
             new ClassCentral\SiteBundle\ClassCentralSiteBundle(),
-            new Symfony\Bundle\DoctrineMigrationsBundle\DoctrineMigrationsBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new ClassCentral\ScraperBundle\ClassCentralScraperBundle(),
         );
 
