4bfe588d8ceb1c463d8f58eb30587fc1315d40fa
Dhawal Shah <dhawalhshah@gmail.com>
2013-11-10 16:54:22 -0800

    Added mailgun dependency

---
+++ src/ClassCentral/SiteBundle/Services/Newsletter.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dhawal
+ * Date: 11/10/13
+ * Time: 10:25 AM
+ */
+
+namespace ClassCentral\SiteBundle\Services;
+
+
+class Newsletter {
+
+} 
---
+++ src/ClassCentral/SiteBundle/Tests/Services/NewsletterTest.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dhawal
+ * Date: 11/10/13
+ * Time: 11:33 AM
+ */ 
