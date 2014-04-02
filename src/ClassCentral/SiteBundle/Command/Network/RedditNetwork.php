17b54d56d513a0ac22f17c7fda30301d83bda186
Dhawal Shah <dhawalhshah@gmail.com>
2014-04-02 00:39:22 -0700

    Added programming newsletter templates

--- src/ClassCentral/SiteBundle/Command/Network/RedditNetwork.php
+++ src/ClassCentral/SiteBundle/Command/Network/RedditNetwork.php
@@ -8,12 +8,12 @@ class RedditNetwork extends NetworkAbstractInterface
 {
     public static $coursesByLevel = array(
         'beginner' => array(
-            442,1586,1578,1325,1046,1481,320,1010,904,831,1580,303,375,640,590, 335, 1341, 441, 4891, 408
+            442,1586,1578,1325,1046,1481,320,1010,904,831,1580,303,375,640,590, 335, 1341, 441, 4891, 408, 1797, 1891
         ),
         'intermediate' => array(
             824,599,616,1176,1470,1188,1585,1205,462,1178,339,1478,1479,1480,328,366,323,
             324,325,364,365,457,455,592, 551, 1299, 1701, 1523, 921, 846, 1457, 1742, 1282,
-            650, 417, 594, 1187, 1737, 1738,1646, 1487,849,475, 1021, 835, 428,359,1152,487,1779,1816,1209,526,340, 724, 764,588,
+            650, 417, 594, 1187, 1737, 1738,1646, 1487,849,475, 1021, 835, 428,359,1152,487,1779,1816,1209,526,340, 724, 764,588, 1748, 374, 1777,1875,531,1238,422,1529, 443, 1206, 1377, 1848, 1865 , 1788, 1881, 795
         ),
         'advanced' => array(
             427,449,414,465,319,326,549,550, 552, 425, 1847
@@ -42,8 +42,8 @@ class RedditNetwork extends NetworkAbstractInterface
     public function beforeOffering()
     {
         // Table header row
-        $this->output->writeln("Course Name|Start Date|Length");
-        $this->output->writeln(":--|:--:|:--:");
+        $this->output->writeln("Course Name|Start Date|Length|Provider|Rating");
+        $this->output->writeln(":--|:--:|:--:|:--:|:--:");
     }
 
 
@@ -80,13 +80,19 @@ class RedditNetwork extends NetworkAbstractInterface
 
         // Rating
         $courseRating = $rs->getRatings($offering->getCourse()->getId());
+        $courseReviews = $rs->getReviewsArray( $offering->getCourse()->getId() );
+        $reviewText = '';
         if($courseRating == 0)
         {
             $courseRating = 'NA';
         }
+        else
+        {
+            $reviewText = sprintf("(%d %s)", $courseReviews['count'], ($courseReviews['count'] == 1 ) ? 'review' : 'reviews');
+        }
         $url = 'https://www.class-central.com'. $this->router->generate('ClassCentralSiteBundle_mooc', array('id' => $offering->getCourse()->getId(), 'slug' => $offering->getCourse()->getSlug()));
         $url .= '#course-reviews';
-        $rating = "[$courseRating]($url)";
+        $rating = "[$courseRating]($url) $reviewText";
 
         $this->output->writeln("$name|$startDate|$length|$initiative|$rating");
     } 
