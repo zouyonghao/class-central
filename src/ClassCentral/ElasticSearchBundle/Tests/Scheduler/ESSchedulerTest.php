66c92c40426a471eac959575e726d8dddc8fe26c
Dhawal Shah <dhawalhshah@gmail.com>
2014-04-23 14:04:39 -0700

    Fix the scheduler unit test

--- src/ClassCentral/ElasticSearchBundle/Tests/Scheduler/ESSchedulerTest.php
+++ src/ClassCentral/ElasticSearchBundle/Tests/Scheduler/ESSchedulerTest.php
@@ -16,6 +16,8 @@ class ESSchedulerTest extends \PHPUnit_Framework_TestCase {
 
     public function testScheduler()
     {
+        $container = $this->getContainerMock();
+
         // Mock the indexer
         $indexer = $this
             ->getMockBuilder('ClassCentral\ElasticSearchBundle\Indexer', array('index'))
@@ -26,15 +28,8 @@ class ESSchedulerTest extends \PHPUnit_Framework_TestCase {
             ->with( $this->containsOnlyInstancesOf('ClassCentral\ElasticSearchBundle\Scheduler\ESJob'), $this->anything() )
             ->will( $this->returnValue(true) );
 
-
-        // Mock the container
-        $container = $this
-            ->getMockBuilder('Symfony\Component\DependencyInjection\ContainerInterface' , array('get'))
-            ->disableOriginalConstructor()
-            ->getMock();
-
         $container
-            ->expects( $this->once())
+            ->expects( $this->at(1))
             ->method('get')
             ->with( $this->stringContains('es_indexer'))
             ->will( $this->returnValue($indexer));
@@ -45,5 +40,37 @@ class ESSchedulerTest extends \PHPUnit_Framework_TestCase {
         $id = $scheduler->schedule( new \DateTime(), 'email', 'ClassName', array());
 
         $this->assertNotEmpty( $id );
+    }
+
+    private function getContainerMock()
+    {
+        // Mock the logger
+
+        $logger = $this
+            ->getMockBuilder( 'Monolog\Logger', array('info', 'error') )
+            ->disableOriginalConstructor()
+            ->getMock();
+        $logger
+            ->expects( $this->any() )
+            ->method( "info" )
+            ->will( $this->returnValue(true));
+        $logger
+            ->expects( $this->any() )
+            ->method( "error" )
+            ->will( $this->returnValue(true));
+
+        // Mock the container
+        $container = $this
+            ->getMockBuilder('Symfony\Component\DependencyInjection\ContainerInterface' , array('get'))
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $container
+            ->expects( $this->at(0) )
+            ->method('get')
+            ->with( 'monolog.logger.scheduler' )
+            ->will( $this->returnValue($logger));
+
+        return $container;
     }
 } 
