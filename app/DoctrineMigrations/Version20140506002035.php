0159437681d18a22ce26b9461b8de0ddd041de17
dhawalhshah <dhawalhshah@gmail.com>
2014-05-06 01:39:12 -0700

    Add a migration to create spotlight table

---
+++ app/DoctrineMigrations/Version20140506002035.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Application\Migrations;
+
+use ClassCentral\SiteBundle\Entity\Spotlight;
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Create a table for managing the spotlight section
+ */
+class Version20140506002035 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        $con = $this->connection;
+        // this up() migration is auto-generated, please modify it to your needs
+        $con->executeQuery(
+            "CREATE TABLE IF NOT EXISTS `spotlights` (
+              `id` INT NOT NULL AUTO_INCREMENT,
+              `position` INT NOT NULL,
+              `title` TEXT NOT NULL,
+              `description` TEXT NOT NULL,
+              `url` TEXT NOT NULL,
+              `image_url` TEXT NOT NULL,
+              `type` INT NOT NULL,
+              PRIMARY KEY (`id`))
+            ENGINE = InnoDB;"
+        );
+
+        // Insert dummy data
+       for($position = 1; $position <= 10; $position++)
+       {
+           $con->insert('spotlights',array(
+                'position' => $position,
+                'title' => 'Spotlight title #' . $position,
+                'description' => 'Welcome to the world of tomorrow',
+                'url' => 'https://www.class-central.com',
+                'image_url' => 'https://lorempixel.com/198/160/',
+                'type' => Spotlight::SPOTLIGHT_TYPE_DEMO
+           ));
+       }
+
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
---
+++ src/ClassCentral/SiteBundle/Entity/Spotlight.php
@@ -0,0 +1,196 @@
+<?php
+
+namespace ClassCentral\SiteBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Spotlight
+ */
+class Spotlight
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var integer
+     */
+    private $position;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @var string
+     */
+    private $description;
+
+    /**
+     * @var string
+     */
+    private $url;
+
+    /**
+     * @var string
+     */
+    private $imageUrl;
+
+    const SPOTLIGHT_TYPE_DEMO = 1; // Only show in dev
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set position
+     *
+     * @param integer $position
+     * @return Spotlight
+     */
+    public function setPosition($position)
+    {
+        $this->position = $position;
+    
+        return $this;
+    }
+
+    /**
+     * Get position
+     *
+     * @return integer 
+     */
+    public function getPosition()
+    {
+        return $this->position;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Spotlight
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Spotlight
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set url
+     *
+     * @param string $url
+     * @return Spotlight
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+    
+        return $this;
+    }
+
+    /**
+     * Get url
+     *
+     * @return string 
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Set imageUrl
+     *
+     * @param string $imageUrl
+     * @return Spotlight
+     */
+    public function setImageUrl($imageUrl)
+    {
+        $this->imageUrl = $imageUrl;
+    
+        return $this;
+    }
+
+    /**
+     * Get imageUrl
+     *
+     * @return string 
+     */
+    public function getImageUrl()
+    {
+        return $this->imageUrl;
+    }
+    /**
+     * @var integer
+     */
+    private $type;
+
+
+    /**
+     * Set type
+     *
+     * @param integer $type
+     * @return Spotlight
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+    
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return integer 
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+}
