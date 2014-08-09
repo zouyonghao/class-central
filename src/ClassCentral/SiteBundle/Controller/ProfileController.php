d92fe42f4b756c8d195665cdd5d6adfbe5898d00
dhawalhshah <dhawalhshah@gmail.com>
2014-08-09 15:48:15 -0700

    Added a CRUD interface for Profile

---
+++ src/ClassCentral/SiteBundle/Controller/ProfileController.php
@@ -0,0 +1,185 @@
+<?php
+
+namespace ClassCentral\SiteBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use ClassCentral\SiteBundle\Entity\Profile;
+use ClassCentral\SiteBundle\Form\ProfileType;
+
+/**
+ * Profile controller.
+ *
+ */
+class ProfileController extends Controller
+{
+
+    /**
+     * Lists all Profile entities.
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('ClassCentralSiteBundle:Profile')->findAll();
+
+        return $this->render('ClassCentralSiteBundle:Profile:index.html.twig', array(
+            'entities' => $entities,
+        ));
+    }
+    /**
+     * Creates a new Profile entity.
+     *
+     */
+    public function createAction(Request $request)
+    {
+        $entity  = new Profile();
+        $form = $this->createForm(new ProfileType(), $entity);
+        $form->bind($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('profile_show', array('id' => $entity->getId())));
+        }
+
+        return $this->render('ClassCentralSiteBundle:Profile:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to create a new Profile entity.
+     *
+     */
+    public function newAction()
+    {
+        $entity = new Profile();
+        $form   = $this->createForm(new ProfileType(), $entity);
+
+        return $this->render('ClassCentralSiteBundle:Profile:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a Profile entity.
+     *
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('ClassCentralSiteBundle:Profile')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Profile entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('ClassCentralSiteBundle:Profile:show.html.twig', array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Profile entity.
+     *
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('ClassCentralSiteBundle:Profile')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Profile entity.');
+        }
+
+        $editForm = $this->createForm(new ProfileType(), $entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('ClassCentralSiteBundle:Profile:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Edits an existing Profile entity.
+     *
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('ClassCentralSiteBundle:Profile')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Profile entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createForm(new ProfileType(), $entity);
+        $editForm->bind($request);
+
+        if ($editForm->isValid()) {
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('profile_edit', array('id' => $id)));
+        }
+
+        return $this->render('ClassCentralSiteBundle:Profile:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+    /**
+     * Deletes a Profile entity.
+     *
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $form = $this->createDeleteForm($id);
+        $form->bind($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('ClassCentralSiteBundle:Profile')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find Profile entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('profile'));
+    }
+
+    /**
+     * Creates a form to delete a Profile entity by id.
+     *
+     * @param mixed $id The entity id
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder(array('id' => $id))
+            ->add('id', 'hidden')
+            ->getForm()
+        ;
+    }
+}
