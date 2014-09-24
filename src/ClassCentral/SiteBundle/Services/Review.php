229a50f8be1e771b0676a86304fc6b7e8e7d0f6c
dhawalhshah <dhawalhshah@gmail.com>
2014-09-23 17:36:36 -0700

    minor tweaks

--- src/ClassCentral/SiteBundle/Services/Review.php
+++ src/ClassCentral/SiteBundle/Services/Review.php
@@ -9,7 +9,6 @@
 namespace ClassCentral\SiteBundle\Services;
 
 use ClassCentral\SiteBundle\Entity\Course;
-use ClassCentral\SiteBundle\Entity\User;
 use ClassCentral\SiteBundle\Entity\UserCourse;
 use ClassCentral\SiteBundle\Utility\ReviewUtility;
 use ClassCentral\SiteBundle\Entity\Review as ReviewEntity;
