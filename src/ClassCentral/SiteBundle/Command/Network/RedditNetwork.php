4b9cd5a59b1cb6bf81764f416928f2501ecc6129
dhawalhshah <dhawalhshah@gmail.com>
2014-11-05 10:22:32 -0800

    Added november programming newsletter

--- src/ClassCentral/SiteBundle/Command/Network/RedditNetwork.php
+++ src/ClassCentral/SiteBundle/Command/Network/RedditNetwork.php
@@ -10,7 +10,8 @@ class RedditNetwork extends NetworkAbstractInterface
         'beginner' => array(
             442,1586,1578,1325,1046,1481,320,1010,904,831,1580,303,375,640,590, 335, 1341, 441, 4891, 408, 1797, 1891, 2013, 1952, 2013,
             306, 529, 1983, 1850,1957, 2042,1821,2175,1650,732,553,1857,1727,
-            615,1894, 1349, 2298, 1651,2195, 2486, 2659,2660, 2661, 2129, 2448, 1243
+            615,1894, 1349, 2298, 1651,2195, 2486, 2659,2660, 2661, 2129, 2448, 1243,
+            2630
         ),
         'intermediate' => array(
             824,599,616,1176,1470,1188,1585,1205,462,1178,339,1478,1479,1480,328,366,323,
@@ -20,7 +21,8 @@ class RedditNetwork extends NetworkAbstractInterface
             1948,533, 489, 1906, 451, 453, 426, 600, 2056, 1346, 558, 1724, 2109,2110,2111,
             2189,1715,1716,1717,1718,1719,1720,1712,1713,1714,1704,2211,458, 1806,2212,
             2215, 2335,2336, 2067,1053,1725,2214, 777, 1345,1347, 2038, 376, 2340, 342,
-            1766, 2406, 745, 2360, 1240, 1348, 2427, 2658, 2503, 452, 2489, 548, 429, 2147, 1728, 2420, 2144
+            1766, 2406, 745, 2360, 1240, 1348, 2427, 2658, 2503, 452, 2489, 548, 429, 2147, 1728, 2420, 2144,
+            2716, 2244, 1730, 2861, 2445
         ),
         'advanced' => array(
             427,449,414,465,319,326,549,550, 552, 425, 1847,1848, 1849, 2018, 2458
@@ -98,7 +100,7 @@ class RedditNetwork extends NetworkAbstractInterface
             $reviewText = sprintf("(%d %s)", $courseReviews['count'], ($courseReviews['count'] == 1 ) ? 'review' : 'reviews');
         }
         $url = 'https://www.class-central.com'. $this->router->generate('ClassCentralSiteBundle_mooc', array('id' => $offering->getCourse()->getId(), 'slug' => $offering->getCourse()->getSlug()));
-        $url .= '#course-reviews';
+        $url .= '#course-all-reviews';
         $rating = "[$courseRating]($url) $reviewText";
 
         $this->output->writeln("$name|$startDate|$length|$initiative|$rating");
