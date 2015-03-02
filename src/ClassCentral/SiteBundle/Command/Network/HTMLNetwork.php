d490c21f9b9155bf8c64ae7e258ad612dd3015d0
dhawalhshah <dhawalhshah@gmail.com>
2015-03-01 23:28:14 -0800

    Tweaked output format for Course Report

--- src/ClassCentral/SiteBundle/Command/Network/HTMLNetwork.php
+++ src/ClassCentral/SiteBundle/Command/Network/HTMLNetwork.php
@@ -9,16 +9,7 @@ class HTMLNetwork extends NetworkAbstractInterface
 {
     public function outInitiative( $stream , $offeringCount)
     {
-//        if($initiative == null)
-//        {
-//            $name = 'Others';
-//            $url = '';
-//        }
-//        else
-//        {
-//            $name = $initiative->getName();
-//            $url = $initiative->getUrl();
-//        }
+
           $name   = $stream->getName();
           $url = "https://www.class-central.com/stream/". $stream->getSlug();
 
@@ -33,40 +24,7 @@ class HTMLNetwork extends NetworkAbstractInterface
 
     public function outOffering(Offering $offering)
     {
-        // Print the title line
-        $titleLine = $offering->getName();
-        $url = $offering->getUrl();
-        $url = 'https://www.class-central.com'. $this->router->generate('ClassCentralSiteBundle_mooc', array('id' => $offering->getCourse()->getId(), 'slug' => $offering->getCourse()->getSlug()));
-        $this->output->writeln("<a href='$url'>$titleLine</a><br/>");
-
-        $secondLine = array();
-        if ($offering->getStatus() == Offering::START_DATES_KNOWN)
-        {
-            $secondLine[] = $offering->getStartDate()->format('M jS');
-        }
-
-        $initiative = $offering->getInitiative();
-        if($initiative == null)
-        {
-            $name = 'Others';
-            $url = '';
-        }
-        else
-        {
-            $name = $initiative->getName();
-            $url = $initiative->getUrl();
-        }
-
-        // Print out the course length. Exclude Udacity because course length is same
-        if (  $initiative != 'UDACITY' && $offering->getLength() != 0)
-        {
-            $secondLine[] = $offering->getLength() . " weeks long";
-        }
-
-        $secondLine[] = $name;
-        if (!empty($secondLine))
-        {
-            $this->output->writeln("<i>" . implode(' | ', $secondLine) . "</i><br/>");
-        }
+        $formatter = $this->container->get('course_formatter');
+        echo $formatter->blogFormat( $offering->getCourse() );
     }
 }
