9835ba9ea03ca93faeb5df4e3964dfd720603abd
dhawalhshah <dhawalhshah@gmail.com>
2015-03-22 20:18:10 -0700

    Add a command to review all un summarized reviews

--- src/ClassCentral/SiteBundle/Command/SummarizeReviewCommand.php
+++ src/ClassCentral/SiteBundle/Command/SummarizeReviewCommand.php
@@ -17,13 +17,20 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class SummarizeReviewCommand extends ContainerAwareCommand {
 
+
+    /**
+     * Review Type:
+     * course - summarizes a review for the course
+     * review - summarizes a review
+     * all    - summarizes all un-summarized reviews
+     */
     protected function configure()
     {
         $this
             ->setName('classcentral:reviews:summarize')
             ->setDescription('Summarize a review or all the reviews for a course')
-            ->addArgument('type', InputArgument::REQUIRED,"course or review")
-            ->addArgument('id',InputArgument::REQUIRED,"course id or review id")
+            ->addArgument('type', InputArgument::REQUIRED,"course, review, or all")
+            ->addArgument('id',InputArgument::REQUIRED,"course id or review id. ignored for all")
         ;
     }
 
@@ -34,9 +41,9 @@ class SummarizeReviewCommand extends ContainerAwareCommand {
         $type = strtolower($input->getArgument('type'));
         $id   =  intval( $input->getArgument('id') );
 
-        if( !in_array( $type, array('course','review')) )
+        if( !in_array( $type, array('course','review','all')) )
         {
-            $output->writeln( "<error>type should be either review or course</error>" );
+            $output->writeln( "<error>type should be either review, course, or all</error>" );
             return;
         }
 
@@ -60,7 +67,7 @@ class SummarizeReviewCommand extends ContainerAwareCommand {
             }
 
         }
-        else
+        else if( $type == 'review')
         {
             // type is review
             $review = $this->getContainer()
@@ -75,19 +82,51 @@ class SummarizeReviewCommand extends ContainerAwareCommand {
             }
             else
             {
-                $response = $reviewService->summarizeReview( $review );
-                switch ($response)
-                {
-                    case Review::REVIEW_ALREADY_SUMMARIZED_OR_EMPTY_TEXT:
-                        $output->writeln( "Review already summarized");
-                        break;
-                    case Review::REVIEW_SUMMARY_FAILED:
-                        $output->writeln("Review summary failed");
-                        break;
-                    default:
-                        $output->writeln("$response Summaries saved for review with id $id ");
-                }
+                $this->summarizeReview($review, $output);
             }
+
+
+        }
+        else if( $type == 'all')
+        {
+            // Summarize all reviews
+
+            // Find all reviews which need to be summarized
+            $query = $this->getContainer()->get('doctrine')->getManager()->createQueryBuilder();
+            $query
+                ->add('select', 'r')
+                ->add('from', 'ClassCentralSiteBundle:Review r')
+                ->where('r.reviewSummary is  NULL AND LENGTH(r.review) > 30 AND r.status = :status')
+                ->setParameter('status',\ClassCentral\SiteBundle\Entity\Review::REVIEW_STATUS_APPROVED)
+            ;
+            $result = $query->getQuery()->getResult();
+            $reviewsToBeSummarized = count( $result );
+            $output->writeln("<info>Summarising $reviewsToBeSummarized reviews</info>");
+            foreach($result as $review)
+            {
+                $this->summarizeReview($review, $output);
+            }
+
+
         }
     }
+
+    private function summarizeReview(\ClassCentral\SiteBundle\Entity\Review $review, OutputInterface $output )
+    {
+        $reviewService = $this->getContainer()->get('review');
+        $response = $reviewService->summarizeReview( $review );
+
+        switch ($response)
+        {
+            case Review::REVIEW_ALREADY_SUMMARIZED_OR_EMPTY_TEXT:
+                $output->writeln( "Review already summarized");
+                break;
+            case Review::REVIEW_SUMMARY_FAILED:
+                $output->writeln("Review summary failed for id {$review->getId()} ");
+                break;
+            default:
+                $output->writeln("$response Summaries saved for review with id {$review->getId()} ");
+        }
+    }
+
 } 
