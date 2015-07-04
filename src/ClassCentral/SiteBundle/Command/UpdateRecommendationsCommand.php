f3efa53e9fd67ed02d91a79c57c5be1a321c2ba9
dhawalhshah <dhawalhshah@gmail.com>
2015-07-03 19:00:46 -0700

    Related courses are now generated based on the people interested in a particular course are most interested in other coures

--- src/ClassCentral/SiteBundle/Command/UpdateRecommendationsCommand.php
+++ src/ClassCentral/SiteBundle/Command/UpdateRecommendationsCommand.php
@@ -10,6 +10,8 @@ namespace ClassCentral\SiteBundle\Command;
 
 
 use ClassCentral\SiteBundle\Entity\CourseRecommendation;
+use ClassCentral\SiteBundle\Entity\CourseStatus;
+use Doctrine\ORM\Query\ResultSetMapping;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -40,73 +42,32 @@ class UpdateRecommendationsCommand extends ContainerAwareCommand {
         $conn = $em->getConnection()
                 ->exec("TRUNCATE courses_recommendations");
 
-        // Build a course map
-        $this->buildCoursesMap();
-
-        if (($handle = fopen("extras/recommendations.csv", "r")) !== FALSE) {
-            fgetcsv($handle, 1000, ","); // Skip the first row
-            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-                $num = count($data);
-                if($num != 5)
-                {
-                    // No course recommendations found
-                    continue;
-                }
-
-                $course_id = $data[1];
-                $recommendations = explode(',', $data[3]);
-                $recommendations_score = explode(',', $data[2]);
-
-                $this->updateRecommendations($course_id,$recommendations,$recommendations_score);
-                //echo "$course_id ||| " . print_r($recommendations, true) . " ||| " . print_r($recommendations_score, true) . "\n";
-            }
-            fclose($handle);
-        }
-
-        $em->flush();
-    }
-
-    private function updateRecommendations($courseId, array $recommendations, array $score)
-    {
-        $em = $this->getContainer()->get('doctrine')->getManager();
-
-        // Get the course
-        $course = $this->courses[$courseId];
-
-        for($i = 0; $i < count($recommendations); $i++)
+        $courses = $em->getRepository('ClassCentralSiteBundle:Course')->findAll();
+        foreach($courses as $course)
         {
-            $rc = $this->courses[trim($recommendations[$i])]; // Recommended course
-            $rcScore = trim($score[$i]);
+            if($course->getStatus() == CourseStatus::NOT_AVAILABLE) continue;
 
-            // Ignore courses with less than a score of 15. Since the recommendations
-            // are in order, any other score after this would be also less than 15
-            if($rcScore < self::RECOMMENDATIONS_SCORE)
+            $output->writeln($course->getName());
+            // 1. Get all the users interested in the course
+            // 2. Get the most popular courses for the interested courses
+            $rsm = new ResultSetMapping();
+            $rsm->addScalarResult('course_id','course_id');
+            $results = $em->createNativeQuery("SELECT course_id, count(*) FROM users_courses WHERE user_id IN(SELECT user_id FROM users_courses WHERE course_id = {$course->getId()}) GROUP BY course_id ORDER BY count(*) DESC LIMIT 10;", $rsm)->getResult();
+            $position = 1;
+            array_shift($results); // Remove the first element which is the course itself
+            foreach($results as $result)
             {
-                break;
+                $rc =  $em->getRepository('ClassCentralSiteBundle:Course')->find($result['course_id']);
+                if(!$rc) continue;
+                $r = new CourseRecommendation();
+                $r->setCourse($course);
+                $r->setRecommendedCourse($rc);
+                $r->setPosition( $position );
+                $em->persist($r);
+                $position++;
             }
-
-            $r = new CourseRecommendation();
-            $r->setCourse($course);
-            $r->setRecommendedCourse($rc);
-            $r->setPosition($i+1); // So as to start with 1,2,3
-
-            $em->persist($r);
+            $em->flush();
         }
 
-        return;
-
-    }
-
-    private function buildCoursesMap()
-    {
-        $allCourses = $this->getContainer()->get('doctrine')
-            ->getManager()
-            ->getRepository('ClassCentralSiteBundle:Course')
-            ->findAll();
-
-        foreach($allCourses as $course)
-        {
-            $this->courses[$course->getId()] = $course;
-        }
     }
 } 
