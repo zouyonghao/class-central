0170e5858890207229046e26f2cf80bb46011666
dhawalhshah <dhawalhshah@gmail.com>
2015-12-31 17:38:55 -0800

    Follow entity and mapping

---
+++ src/ClassCentral/SiteBundle/Entity/Follow.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace ClassCentral\SiteBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Follow
+ */
+class Follow
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $item;
+
+    /**
+     * @var integer
+     */
+    private $itemId;
+
+    /**
+     * @var \DateTime
+     */
+    private $created;
+
+    public function __construct()
+    {
+        $this->created = new \DateTime();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set item
+     *
+     * @param string $item
+     * @return Follow
+     */
+    public function setItem($item)
+    {
+        $this->item = $item;
+    
+        return $this;
+    }
+
+    /**
+     * Get item
+     *
+     * @return string 
+     */
+    public function getItem()
+    {
+        return $this->item;
+    }
+
+    /**
+     * Set itemId
+     *
+     * @param integer $itemId
+     * @return Follow
+     */
+    public function setItemId($itemId)
+    {
+        $this->itemId = $itemId;
+    
+        return $this;
+    }
+
+    /**
+     * Get itemId
+     *
+     * @return integer 
+     */
+    public function getItemId()
+    {
+        return $this->itemId;
+    }
+
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     * @return Follow
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+    
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return \DateTime 
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+}
