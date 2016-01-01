284681f6718c963f858d0460cb645caeef55cae3
dhawalhshah <dhawalhshah@gmail.com>
2015-12-31 23:38:50 -0800

    follow controller with follow subject route

---
+++ src/ClassCentral/SiteBundle/Controller/FollowController.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dhawal
+ * Date: 12/31/15
+ * Time: 10:46 PM
+ */
+
+namespace ClassCentral\SiteBundle\Controller;
+
+
+use ClassCentral\SiteBundle\Utility\UniversalHelper;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class FollowController extends Controller
+{
+
+    public function followSubjectAction(Request $request, $subjectId)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $followService = $this->get('follow');
+
+        $user = $this->get('security.context')->getToken()->getUser();
+        if($user)
+        {
+
+            $subject = $em->getRepository('ClassCentralSiteBundle:Stream')->find($subjectId);
+            if($subject)
+            {
+                $f = $followService->follow($user,$subject);
+                if($f)
+                {
+                    return UniversalHelper::getAjaxResponse(true);
+                }
+                else
+                {
+                    return UniversalHelper::getAjaxResponse (false, "Follow Failed");
+                }
+            }
+            else
+            {
+                return UniversalHelper::getAjaxResponse (false, "Subject not found");
+            }
+        }
+        else
+        {
+            // No logged in user
+            return UniversalHelper::getAjaxResponse (false, "User is not logged in");
+        }
+    }
+}
