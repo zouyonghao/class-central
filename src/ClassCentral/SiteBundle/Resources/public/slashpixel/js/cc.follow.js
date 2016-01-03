ff71ed6c5eabf9ab67993b0b6ba6024f17f22a00
dhawalhshah <dhawalhshah@gmail.com>
2016-01-03 12:30:05 -0800

    prompt that ask users to personalize their education.

--- src/ClassCentral/SiteBundle/Resources/public/slashpixel/js/cc.follow.js
+++ src/ClassCentral/SiteBundle/Resources/public/slashpixel/js/cc.follow.js
@@ -8,6 +8,7 @@ CC.Class['Follow'] = (function(){
 
     function init() {
         $('.btn-follow-item').click(followClicked);
+        personalizationPrompt();
     }
 
     function followClicked(e) {
@@ -80,6 +81,23 @@ CC.Class['Follow'] = (function(){
                 }
             }
         });
+    }
+
+    function personalizationPrompt() {
+
+        setTimeout(function(){
+            swal({
+                title: "Meet Your Next Favourite Course",
+                text: "Get regular email updates of new and upcoming courses by following subjects, universities, and course providers.",
+                type: "info",
+                showCancelButton: true,
+                confirmButtonColor: "#043BFF",
+                confirmButtonText: "Start Personalizing Now",
+                closeOnConfirm: false
+            }, function () {
+               window.location.href = '/user/follows';
+            });
+        },5000);
     }
 
     return {
