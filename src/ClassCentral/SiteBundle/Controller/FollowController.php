4a87adf86e93891668c68beab8fa0e7ced400ac0
dhawalhshah <dhawalhshah@gmail.com>
2016-01-05 15:04:59 -0800

    controller action to unfollow

--- src/ClassCentral/SiteBundle/Controller/FollowController.php
+++ src/ClassCentral/SiteBundle/Controller/FollowController.php
@@ -19,7 +19,6 @@ class FollowController extends Controller
 
     public function followAction(Request $request, $item,$itemId)
     {
-        $em = $this->getDoctrine()->getManager();
         $followService = $this->get('follow');
         $userSession = $this->get('user_session');
 
@@ -36,6 +35,34 @@ class FollowController extends Controller
             else
             {
                 return UniversalHelper::getAjaxResponse (false, "Follow Failed");
+            }
+
+        }
+        else
+        {
+            // No logged in user
+            return UniversalHelper::getAjaxResponse (false, "User is not logged in");
+        }
+    }
+
+    public function unfollowAction(Request $request, $item, $itemId)
+    {
+        $followService = $this->get('follow');
+        $userSession = $this->get('user_session');
+
+        $user = $this->get('security.context')->getToken()->getUser();
+        if($user)
+        {
+            $f = $followService->unFollowUsingItemInfo($user,$item,$itemId);
+            if($f)
+            {
+                // Update User Session
+                $userSession->saveFollowInformation();
+                return UniversalHelper::getAjaxResponse(true);
+            }
+            else
+            {
+                return UniversalHelper::getAjaxResponse (false, "Unfollowing Failed");
             }
 
         }
