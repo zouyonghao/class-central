5a4bfc843475917e5cae9651ca8f9c1f7b6c760c
dhawalhshah <dhawalhshah@gmail.com>
2016-03-19 20:25:32 -0700

    career and categroy entities

---
+++ src/ClassCentral/SiteBundle/Entity/Career.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace ClassCentral\SiteBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Career
+ */
+class Career
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $slug;
+
+    /**
+     * @var \ClassCentral\SiteBundle\Entity\Category
+     */
+    private $category;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Career
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set slug
+     *
+     * @param string $slug
+     * @return Career
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+    
+        return $this;
+    }
+
+    /**
+     * Get slug
+     *
+     * @return string 
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    /**
+     * Set category
+     *
+     * @param \ClassCentral\SiteBundle\Entity\Category $category
+     * @return Career
+     */
+    public function setCategory(\ClassCentral\SiteBundle\Entity\Category $category = null)
+    {
+        $this->category = $category;
+    
+        return $this;
+    }
+
+    /**
+     * Get category
+     *
+     * @return \ClassCentral\SiteBundle\Entity\Category 
+     */
+    public function getCategory()
+    {
+        return $this->category;
+    }
+}
