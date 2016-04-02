3857dcc0664f6c9abc9e067f6941bd7c5eb1c242
dhawalhshah <dhawalhshah@gmail.com>
2016-04-01 20:08:00 -0700

    initial controller and js file commit for meet your next course

---
+++ src/ClassCentral/SiteBundle/Controller/NextCourseWizardController.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dhawal
+ * Date: 4/1/16
+ * Time: 7:57 PM
+ */
+
+namespace ClassCentral\SiteBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Steps for Meet your next course button
+ * Class NextCourseWizardController
+ * @package ClassCentral\SiteBundle\Controller
+ */
+class NextCourseWizardController extends Controller
+{
+    public function stepPickSubjectsAction(Request $request)
+    {
+
+    }
+
+    public function stepPickProvidersAction(Request $request)
+    {
+
+    }
+
+    public function stepLoadingtAction(Request $request)
+    {
+
+    }
+
+
+    public function nextCourseAction(Request $request)
+    {
+
+    }
+}
---
+++ src/ClassCentral/SiteBundle/Resources/public/slashpixel/js/cc.nextcourse.js
@@ -0,0 +1,17 @@
+var CC = CC || {
+        Class : {}
+    }
+
+
+CC.Class['NextCourse'] = (function(){
+
+    function init() {
+
+    }
+
+    return {
+        init: init
+    }
+})();
+
+CC.Class['NextCourse'].init();
