239cbff881385f16a8e7831d1cd9ed3d7fac4abe
dhawalhshah <dhawalhshah@gmail.com>
2016-06-01 21:31:04 -0700

    can create kadenze courses

--- src/ClassCentral/ScraperBundle/Scraper/Kadenze/Scraper.php
+++ src/ClassCentral/ScraperBundle/Scraper/Kadenze/Scraper.php
@@ -2,13 +2,15 @@
 
 namespace ClassCentral\ScraperBundle\Scraper\Kadenze;
 
+use ClassCentral\SiteBundle\Entity\Course;
+
 class Scraper extends \ClassCentral\ScraperBundle\Scraper\ScraperAbstractInterface
 {
 
     const COURSES_API_ENDPOINT = 'https://www.kadenze.com/catalog.json?source=classcentral';
 
     private $courseFields = array(
-        'Url', 'Description', 'DurationMin','DurationMax','Name','LongDescription','Certificate','WorkloadType','CertificatePrice'
+        'Url', 'Description','Name', 'ShortName','Description','LongDescription','Certificate','CertificatePrice'
     );
 
     private $offeringFields = array(
@@ -20,16 +22,111 @@ class Scraper extends \ClassCentral\ScraperBundle\Scraper\ScraperAbstractInterfa
         $em = $this->getManager();
         $kCourses = json_decode(file_get_contents( self::COURSES_API_ENDPOINT ), true );
         $coursesChanged = array();
+        $courseService = $this->container->get('course');
+
         foreach($kCourses as $kcourse)
         {
-            $this->out($kcourse['name']);
+            $course =  $this->getCourseEntity( $kcourse );
+            $dbCourse = $this->dbHelper->getCourseByShortName( $course->getShortName() );
+
+            if(!$dbCourse)
+            {
+                $dbCourse = $this->dbHelper->findCourseByName( $course->getName(), $this->initiative);
+            }
+
+            if(empty($dbCourse))
+            {
+                if($this->doCreate())
+                {
+                    $this->out("NEW COURSE - " . $course->getName());
+
+                    // NEW COURSE
+                    if ($this->doModify())
+                    {
+                        $em->persist($course);
+                        $em->flush();
+
+                        $this->dbHelper->sendNewCourseToSlack( $course, $this->initiative );
+
+                        if( $kcourse['logo'] )
+                        {
+                            $courseService->uploadImageIfNecessary( $kcourse['logo'], $course);
+                        }
+                    }
+                    $courseChanged = true;
+
+                }
+            }
+            else
+            {
+                // Check if any fields are modified
+                $courseModified = false;
+                $changedFields = array(); // To keep track of fields that have changed
+                foreach($this->courseFields as $field)
+                {
+                    $getter = 'get' . $field;
+                    $setter = 'set' . $field;
+                    if($course->$getter() != $dbCourse->$getter())
+                    {
+                        $courseModified = true;
+
+                        // Add the changed field to the changedFields array
+                        $changed = array();
+                        $changed['field'] = $field;
+                        $changed['old'] =$dbCourse->$getter();
+                        $changed['new'] = $course->$getter();
+                        $changedFields[] = $changed;
+
+                        $dbCourse->$setter($course->$getter());
+                    }
+
+                }
+
+                if($courseModified && $this->doUpdate())
+                {
+                    //$this->out( "Database course changed " . $dbCourse->getName());
+                    // Course has been modified
+                    $this->out("UPDATE COURSE - " . $dbCourse->getName() . " - ". $dbCourse->getId());
+                    $this->dbHelper->outputChangedFields($changedFields);
+                    if ($this->doModify())
+                    {
+                        $em->persist($dbCourse);
+                        $em->flush();
+
+                        if( $kcourse['logo'] )
+                        {
+                            $courseService->uploadImageIfNecessary( $kcourse['logo'], $dbCourse);
+                        }
+                    }
+                    $courseChanged = true;
+                }
+
+                $course = $dbCourse;
+            }
         }
 
     }
 
     public function getCourseEntity($course)
     {
-        $c = new \ClassCentral\SiteBundle\Entity\Course();
+        $defaultStream = $this->dbHelper->getStreamBySlug('art-and-design');
+        $langMap = $this->dbHelper->getLanguageMap();
+        $defaultLanguage = $langMap[ 'English' ];
 
+        $c = new \ClassCentral\SiteBundle\Entity\Course();
+        $c->setName($course['name']);
+        $c->setUrl($course['url']);
+        $c->setInitiative( $this->initiative );
+        $c->setShortName( 'kadenze_'. $course['id'] );
+        $c->setVideoIntro( $course['promo_video']);
+        $c->setLanguage( $defaultLanguage);
+        $c->setStream($defaultStream); // Default to Art and Design
+
+        $c->setDescription( $course['description'] );
+        $c->setLongDescription( $course['description'] );
+        $c->setCertificate(true);
+        $c->setCertificatePrice(Course::PAID_CERTIFICATE);
+
+        return $c;
     }
 }
