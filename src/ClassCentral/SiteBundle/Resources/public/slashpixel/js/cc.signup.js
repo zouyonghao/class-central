fed5ba33e728ba4927078898a86d4d60f2300120
dhawalhshah <dhawalhshah@gmail.com>
2016-06-05 12:57:40 -0700

    fixed onboarding bug

--- src/ClassCentral/SiteBundle/Resources/public/slashpixel/js/cc.signup.js
+++ src/ClassCentral/SiteBundle/Resources/public/slashpixel/js/cc.signup.js
@@ -34,7 +34,7 @@ CC.Class['Signup'] = (function(){
                       // Signup successful. Hide the modal
                       form.parent().parent().parent().parent().parent().parent().modal("hide");
                       // Show the form update
-                      if($('#meet-your-next-course-page')) {
+                      if($('#meet-your-next-course-page').length > 0) {
                           // Skip the first two steps and move to the profile step
                           showOnboardingProfileStep();
                       } else {
