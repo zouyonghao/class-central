6679abe7091cd90080e5b497c53aab2b5b865e47
dhawalhshah <dhawalhshah@gmail.com>
2016-08-26 13:25:32 -0700

    Indexing only 1000 courses at a time to avoid memry issues

--- src/ClassCentral/ElasticSearchBundle/Command/ElasticSearchIndexerCommand.php
+++ src/ClassCentral/ElasticSearchBundle/Command/ElasticSearchIndexerCommand.php
@@ -17,6 +17,7 @@ use ClassCentral\SiteBundle\Controller\StreamController;
 use ClassCentral\SiteBundle\Entity\Initiative;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class ElasticSearchIndexerCommand extends ContainerAwareCommand{
@@ -24,7 +25,10 @@ class ElasticSearchIndexerCommand extends ContainerAwareCommand{
     protected function configure()
     {
         $this->setName("classcentral:elasticsearch:indexer")
-             ->setDescription("Indexes documents into elastic search");
+             ->setDescription("Indexes documents into elastic search")
+             ->addOption('courses', null,InputOption::VALUE_OPTIONAL,"Yes/No - Index courses vs Index the rest. Defaults to yes")
+             ->addOption('offset',null, InputOption::VALUE_OPTIONAL,"Offset id for courses. Course ids between offset and offset+1000 are Index ")
+        ;
     }
 
     /**
@@ -38,128 +42,146 @@ class ElasticSearchIndexerCommand extends ContainerAwareCommand{
         $indexer = $this->getContainer()->get('es_indexer');
         $indexer->setContainer($this->getContainer());
         $em = $this->getContainer()->get('doctrine')->getManager();
+        $qb = $em->createQueryBuilder();
         $cache = $this->getContainer()->get('cache');
+
+        $indexCourses = true;
+        if( $input->getOption('courses') == 'No' )
+        {
+            $indexCourses = false;
+        }
+
+        $offset= 1;
+        if(!empty($input->getOption('offset')))
+        {
+            $offset = intval( $input->getOption('offset') );
+        }
 
         /****
          * Index courses
          */
-        $offset = 0;
-        $limit = 100;
-        $count = 0;
-        do {
+
+        if($indexCourses)
+        {
+            $count = 0;
+
             unset($courses);
-            $courses = $this->getContainer()->get('doctrine')->getManager()
-                ->getRepository('ClassCentralSiteBundle:Course')->findBy(
-                    array(),
-                    array(),
-                    100,
-                    $offset
-                );
+            $query = $qb
+                ->select('c')
+                ->from('ClassCentralSiteBundle:Course','c')
+                ->andwhere( $qb->expr()->gt('c.id', $offset) )
+                ->andwhere(  $qb->expr()->lt('c.id', $offset + 1000) )
+                ->getQuery()
+            ;
+
+            $courses = $query->getResult();
             foreach($courses as $course)
             {
                 $indexer->index($course);
                 $count++;
             }
             $output->writeLn("$count courses indexed");
-            $offset += $limit;
-        } while($courses);
-
-        /****
-         * Index Credentials
-         */
-        $credentials = $this->getContainer()->get('doctrine')->getManager()
-            ->getRepository('ClassCentralCredentialBundle:Credential')->findAll();
-
-        foreach($credentials as $credential)
-        {
-            $indexer->index($credential);
         }
-        $output->writeln("All Credentials indexed");
 
-
-        /***
-         * Index languages
-         */
-        $langController = new LanguageController();
-        $languages = $cache->get('language_list_count', array($langController, 'getLanguagesList'),array($this->getContainer()));
-        foreach($languages as $language)
+        else
         {
-            $indexer->index($language);
-        }
-        $output->writeln("All Languages indexed");
 
-        /***
-         * Index universities/institutions
-         */
-        $insController = new InstitutionController();
-        // Get institutions
-        $data = $insController->getInstitutions( $this->getContainer(), false);
-        $institutions = $data['institutions'];
-        // Get Universities
-        $data = $insController->getInstitutions( $this->getContainer(), true);
-        $universities = $data['institutions'];
-        $all = array_merge( $institutions, $universities);
-        foreach($all as $ins)
-        {
-            if( $ins['count'] > 0)
+            /****
+             * Index Credentials
+             */
+            $credentials = $this->getContainer()->get('doctrine')->getManager()
+                ->getRepository('ClassCentralCredentialBundle:Credential')->findAll();
+
+            foreach($credentials as $credential)
             {
-                $i = $em->getRepository('ClassCentralSiteBundle:Institution')->findOneBy( array('slug' => $ins['slug']));
-                $i->setCount( $ins['count'] );
-                $indexer->index($i);
+                $indexer->index($credential);
             }
+            $output->writeln("All Credentials indexed");
 
-        }
-        $output->writeln("All Institutions indexed");
 
-        /****
-         * Index providers
-         */
-        $providerController = new InitiativeController();
-        $data = $providerController->getProvidersList( $this->getContainer() );
-        foreach($data['providers'] as $provider)
-        {
-            if($provider['count'] > 0)
+            /***
+             * Index languages
+             */
+            $langController = new LanguageController();
+            $languages = $cache->get('language_list_count', array($langController, 'getLanguagesList'),array($this->getContainer()));
+            foreach($languages as $language)
             {
-                if($provider['code'] == 'independent')
+                $indexer->index($language);
+            }
+            $output->writeln("All Languages indexed");
+
+            /***
+             * Index universities/institutions
+             */
+            $insController = new InstitutionController();
+            // Get institutions
+            $data = $insController->getInstitutions( $this->getContainer(), false);
+            $institutions = $data['institutions'];
+            // Get Universities
+            $data = $insController->getInstitutions( $this->getContainer(), true);
+            $universities = $data['institutions'];
+            $all = array_merge( $institutions, $universities);
+            foreach($all as $ins)
+            {
+                if( $ins['count'] > 0)
                 {
-                    $p = new Initiative();
-                    $p->setCode('independent');
-                    $p->setName( 'Independent' );
-                }
-                else
-                {
-                    $p = $em->getRepository('ClassCentralSiteBundle:Initiative')->findOneBy( array('code' => $provider['code']));
+                    $i = $em->getRepository('ClassCentralSiteBundle:Institution')->findOneBy( array('slug' => $ins['slug']));
+                    $i->setCount( $ins['count'] );
+                    $indexer->index($i);
                 }
 
-                $p->setCount( $provider['count'] );
-                $indexer->index($p);
             }
-        }
-        $output->writeln("All Providers indexed");
+            $output->writeln("All Institutions indexed");
 
-        /****
-         * Index subjects
-         */
-        $subjectRepository = $em->getRepository('ClassCentralSiteBundle:Stream');
-        $subjects = $cache->get('stream_list_count',
-            array( new StreamController(), 'getSubjectsList'),
-            array( $this->getContainer() )
-        );
-        foreach($subjects['parent'] as $subject)
-        {
-            $indexer->index( $subjectRepository->find($subject['id']) );
-        }
+            /****
+             * Index providers
+             */
+            $providerController = new InitiativeController();
+            $data = $providerController->getProvidersList( $this->getContainer() );
+            foreach($data['providers'] as $provider)
+            {
+                if($provider['count'] > 0)
+                {
+                    if($provider['code'] == 'independent')
+                    {
+                        $p = new Initiative();
+                        $p->setCode('independent');
+                        $p->setName( 'Independent' );
+                    }
+                    else
+                    {
+                        $p = $em->getRepository('ClassCentralSiteBundle:Initiative')->findOneBy( array('code' => $provider['code']));
+                    }
 
-        foreach($subjects['children'] as $childSubjects)
-        {
-            foreach( $childSubjects as $subject)
+                    $p->setCount( $provider['count'] );
+                    $indexer->index($p);
+                }
+            }
+            $output->writeln("All Providers indexed");
+
+            /****
+             * Index subjects
+             */
+            $subjectRepository = $em->getRepository('ClassCentralSiteBundle:Stream');
+            $subjects = $cache->get('stream_list_count',
+                array( new StreamController(), 'getSubjectsList'),
+                array( $this->getContainer() )
+            );
+            foreach($subjects['parent'] as $subject)
             {
                 $indexer->index( $subjectRepository->find($subject['id']) );
             }
+
+            foreach($subjects['children'] as $childSubjects)
+            {
+                foreach( $childSubjects as $subject)
+                {
+                    $indexer->index( $subjectRepository->find($subject['id']) );
+                }
+            }
+
+            $output->writeln("All subjects indexed");
         }
-
-        $output->writeln("All subjects indexed");
-
 
     }
 } 
