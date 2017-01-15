5db47fb14ccfc99470158228a6c72424a81e3716
Dhawal Shah <dhawalhshah@gmail.com>
2017-01-15 14:29:44 -0800

    Fixed follow count algo

--- src/ClassCentral/SiteBundle/Resources/public/slashpixel/js/btn.follow.js
+++ src/ClassCentral/SiteBundle/Resources/public/slashpixel/js/btn.follow.js
@@ -82,18 +82,18 @@
             longHand: num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
         };
 
-        if (num < 1000) {
-            nums.shortHand = str;
+        if (num < 1000000) {
+            nums.shortHand = (num / 1000).toFixed(num % 1000 !== 0) + 'k';
+        } else {
+            nums.shortHand = (num / 1000000).toFixed(num % 1000000 !== 0) + 'M';
         }
 
         if (num < 10000) {
             nums.shortHand = str.charAt(0) + ',' + str.substring(1);
         }
 
-        if (num < 1000000) {
-            nums.shortHand = (num / 1000).toFixed(num % 1000 !== 0) + 'k';
-        } else {
-            nums.shortHand = (num / 1000000).toFixed(num % 1000000 !== 0) + 'M';
+        if (num < 1000) {
+            nums.shortHand = str;
         }
 
         return nums;
