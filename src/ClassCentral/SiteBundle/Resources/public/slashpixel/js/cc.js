648d6ae119ece713a32e0f29f4dad8318bd1d652
Scott Robertson <scott@scottusrobus.com>
2017-06-08 14:39:49 -0700

    Fix credentials. Ensure CC.Credential scope is exposed on the window

--- src/ClassCentral/SiteBundle/Resources/public/slashpixel/js/cc.js
+++ src/ClassCentral/SiteBundle/Resources/public/slashpixel/js/cc.js
@@ -3,6 +3,7 @@ window.CC = { Class: {} };
 window.CC.Class.Utitlies = require("./cc.utilities.js").default;
 window.CC.Class.User = require("./cc.user.js").default;
 
+window.CC.Class.Credential = require("./cc.credential.js").default;
 window.CC.Class.Profile = require("./cc.profile.js").default;
 window.CC.Class.Follow = require("./cc.follow.js").default;
 window.CC.Class.Signup = require("./cc.signup.js").default;
